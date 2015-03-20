447356b77adebf19f6c899aab1472bf2dd478490
HoangLVQ <hoanglvqse90184@fpt.edu>
2015-03-21 02:27:46 +0700

    [21/03/2015] Upload module criterion

---
+++ CriterionLib/src/criterion/ICriterionController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package criterion;
+
+/**
+ *
+ * @author hoanglvq
+ */
+public interface ICriterionController {
+    
+    // check criterion 
+    public void checkCriterion();
+    // add new criterion
+    public void addNewCriterion();
+    // ... 
+    
+}
